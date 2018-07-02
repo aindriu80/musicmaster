65dfc4cc9d31fa3f8382229ea9a2378c6b4aa1dc
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-07-02 17:05:14 +0100

    Preparing a Web Request with Spotify API authentication

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+// import axios from 'axios';
 
 class App extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            query: ''
+            query: "", // my query
+            artist: null  // my response.
         }
     }
 
     search() {
         console.log('this.state', this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        // const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+
+        var accessToken = 'BQA5Q-SAJvUmiuxaqC2kh3ftzMBew7SRnw_DTPlzo1c6WI65fhCxI_d07hILUp1ZeUW7xIb_knT10AD6Fu-7Wk30Ydhs1UJjzU6zUHh_UnIq9XuZoClJA8JXbbG2xJxr67gAS5KT-tOLb4_utCOLNgJi__9B_A&refresh_token=AQC1RadQ1oinrW91TCotd6Oaz6CT1c7E46XtNIs8FI97oE-gn0SKe7Dhecgti_sBY5NsDmeYNxfJqNBrsJ0WvxqXLoaLYu1IYxeNDZ8hPfqHo3RSsE_yELStmhLhZEA1LL4'
+
+        console.log('FETCH_URL', FETCH_URL);
+        var myOptions = {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            },
+            mode: 'cors',
+            cache: 'default'
+        };
+
+        fetch(FETCH_URL, myOptions)
+            .then(response => response.json())
+            .then(json => {
+                const artist = json.artists.items[0];
+                this.setState({ artist });
+            })
+
     }
 
     render() {
+
+        let artist = {
+            name: '',
+            followers: {
+                total: ''
+            }
+        };
+        if (this.state.artist !== null) {
+            artist = this.state.artist;
+        }
+
         return (
             <div className="App">
                 <div className="App-title">Music Master</div>
@@ -38,6 +75,7 @@ class App extends Component {
                 </FormGroup>
                 <div className="Profile">
                     <div>Artist Picture</div>
+
                     <div>Artist Name</div>
                 </div>
                 <div className="Gallery">
